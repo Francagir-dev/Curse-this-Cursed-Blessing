a28b0351e08d0db251116f06854d280a5c421db0
Francisco Carrasco Girbés <60694098+Francagir-dev@users.noreply.github.com>
2022-04-27 12:20:19 +0200

    OptionMenu

---
+++ Assets/_project/Scripts/Options/GraphicOptions.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicOptions : MonoBehaviour
+{
+    private int width,
+        height,
+        qualityIndex,
+        defaultWidth,
+        defaultHeight,
+        defaultResolutionResolutionOptionIndex,
+        _selectedResolutionResolutionOptionIndex;
+
+    private bool fullScreen, vfx;
+    private Resolution[] resolutions;
+
+    [SerializeField] private Toggle vfxToggle, fullScreenToggle;
+
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+
+
+    private void OnEnable()
+    {
+        PopulateDropDownResolutionOptions();
+    }
+
+    public void PopulateDropDownResolutionOptions()
+    {
+        resolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+        resolutionDropdown.ClearOptions();
+        defaultResolutionResolutionOptionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            options.Add(resolutions[i].width + " x " + resolutions[i].height);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                _selectedResolutionResolutionOptionIndex = i;
+                defaultWidth = resolutions[i].width;
+                defaultHeight = resolutions[i].height;
+                defaultResolutionResolutionOptionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = _selectedResolutionResolutionOptionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetQuality(int qualitySettingsIndex)
+    {
+        qualityIndex = qualitySettingsIndex;
+    }
+
+    public void ChangeResolution(int index)
+    {
+        Resolution resolution = resolutions[index];
+        width = resolution.width;
+        height = resolution.height;
+        _selectedResolutionResolutionOptionIndex = index;
+    }
+}
