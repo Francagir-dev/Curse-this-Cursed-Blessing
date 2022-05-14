d6438bb803639ed686dcdf2a4063fba8e6969fc4
Gimeno5 <pablogimeno13@gmail.com>
2022-05-14 23:42:25 +0200

    Crepitar_velas

--- Assets/_project/Scripts/Luces/Crepitar.cs
+++ Assets/_project/Scripts/Luces/Crepitar.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 public class Crepitar : MonoBehaviour
 {
+    public float intensity;
+    HDAdditionalLightData myLight;
+    public float velocity;
+    public float Maxintensity;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        myLight = GetComponent<HDAdditionalLightData>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        myLight.intensity = Mathf.PingPong(Time.time * velocity, Maxintensity) + intensity;
+        Debug.Log(intensity);
     }
 }
